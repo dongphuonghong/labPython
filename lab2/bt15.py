d2f4ad0e0ffb92d022a05a8588c0c59c8bc437e1
dongphuonghongduc <dongphuonghongduc@gmail.com>
2024-11-17 15:56:21 +0700

    edit code bt15 python

--- lab2/bt15.py
+++ lab2/bt15.py
@@ -2,14 +2,13 @@
 #tạo biến a, b, nhập giá trị cho a, b từ bàn phím
 a=float(input("Nhap a: "))
 b=float(input("Nhap b: "))
-#nếu a=0 thì phương trình có dạng: bx=0
-if a==0:
-    #nếu b=0 thì phương trình có vô số nghiệm
-    if b==0:
-        print("Phuong trinh co vo so nghiem")
-    #nếu b!=0 thì phương trình vô nghiệm
-    else:
-        print("Phuong trinh vo nghiem")
-#nếu a!=0 thì phương trình có nghiệm x=-b/a
+#nếu a=0 và b=0 thì phương trình vô số nghiệm
+if a==0 and b==0:
+    print("Phuong trinh vo so nghiem")
+#nếu a=0 và b!khác 0 thì phương trình vô nghiệm
+elif a==0 and b!=0:
+    print("Phuong trinh vo nghiem")
+#nếu akhác 0 thì phương trình có nghiệm x=-b/a
 else:
-    print("Phuong trinh co nghiem x = ", -b/a)  
+    x=-b/a
+    print("Phuong trinh co nghiem x = ", x) 
